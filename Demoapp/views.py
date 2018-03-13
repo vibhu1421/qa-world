ac9e22cbc6459ec1e2c7a6d7c70e04528c9caeb3
vibhu <vibhu1421@gmail.com>
2018-03-13 04:46:03 -0600

    changed html again

--- Demoapp/views.py
+++ Demoapp/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils  import timezone
+from django.contrib.auth.models import User
+
 
 def post_list(request):
-    return render(request, 'blog/post_list.html',{})
+    me=User.objects.get(username='admin')
+    post= Post.objects.filter(author=me)
+    return render(request, 'blog/post_list.html',{'post':post})
     
 
 # Create your views here.
